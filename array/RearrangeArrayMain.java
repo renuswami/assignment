3eaa7a574664038c4a7dbc16231eeb5651665620
renuswami <121007900+renuswami@users.noreply.github.com>
2023-05-03 19:45:08 +0530

    main class of RearrangeArray

---
+++ array/RearrangeArrayMain.java
@@ -0,0 +1,16 @@
+package assignment.java.array;
+
+import java.util.Arrays;
+
+public class RearrangeArrayMain {
+
+	public static void main(String[] args) {
+
+		int arr[] ={1, 2, 3, 4, 5, 6, 7};
+
+		RearrangeArray.rearrangeArray(arr);
+		
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
